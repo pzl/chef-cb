05ed7c041393562e15ded61a088494fc8325404d
pzl <dan@panzarel.la>
2013-11-13 14:40:35 -0500

    git setup

--- cookbooks/git/recipes/default.rb
+++ cookbooks/git/recipes/default.rb
@@ -1,4 +1,17 @@
 package 'git-core'
-#configs
-#bash completion
+
+cookbook_file "/home/dan/.gitconfig" do
+	source ".gitconfig"
+	mode 0600
+	owner "dan"
+	group "dan"
+end
+
+cookbook_file "/home/dan/.gitcompletion.sh" do
+	source ".gitcompletion.sh"
+	mode 0600
+	owner "dan"
+	group "dan"
+end
+
 #private keys?
