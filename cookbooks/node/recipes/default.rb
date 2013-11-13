baaa5a01c10273556e8bbe11e653085cc96908df
pzl <dan@panzarel.la>
2013-11-13 12:26:52 -0500

    compile node, auto-determine newest version

--- cookbooks/node/recipes/default.rb
+++ cookbooks/node/recipes/default.rb
@@ -1,2 +1,15 @@
-NODEV=$(curl -s http://nodejs.org/dist/latest/SHASUMS.txt | head -n 1 | sed 's/.*node-v\([0-9]\+\.[0-9]\+\.[0-9]\+\).*/\1/i')
-cd /tmp; wget http://nodejs.org/dist/latest/node-v$NODEV.tar.gz; tar -xzf node-v$NODEV.tar.gz; cd node-v$NODEV; ./configure; make; sudo make install; cd /tmp; rm -rf node-v$NODEV node-v$NODEV.tar.gz
+execute "install node" do
+	cwd "/tmp"
+	command <<-EOH
+		NODEV=$(curl -s http://nodejs.org/dist/latest/SHASUMS.txt | head -n 1 | sed 's/.*node-v\\([0-9]\\+\\.[0-9]\\+\\.[0-9]\\+\\).*/\\1/i')
+		wget http://nodejs.org/dist/latest/node-v$NODEV.tar.gz
+		tar -xzf node-v$NODEV.tar.gz
+		cd node-v$NODEV
+		./configure
+		make
+		make install
+		cd ..
+		rm -rf node-v$NODEV node-v$NODEV.tar.gz
+	EOH
+	creates "/usr/local/bin/node"
+end
