67efc7a042864429472503887b8d4bdd78a7c595
pzl <dan@panzarel.la>
2013-11-13 17:25:39 -0500

    install google talk plugin for hangouts

--- cookbooks/chrome/recipes/default.rb
+++ cookbooks/chrome/recipes/default.rb
@@ -1,6 +1,7 @@
 #chrome yay
 google_key_url_1='https://dl-ssl.google.com/linux/linux_signing_key.pub'
 google_key_url_2='http://packages.crunchbang.org/waldorf-files/apt-keys/google-chrome.key'
+talkplugin_url='https://dl.google.com/linux/direct/google-talkplugin_current_amd64.deb'
 
 execute "install-google-key" do
 	command "wget -O- #{google_key_url_1} | apt-key add -"
@@ -36,3 +37,15 @@ cookbook_file "/home/dan/.config/google-chrome/Default/Bookmarks" do
 end
 
 #talk plugin.deb
+cookbook_file "/etc/apt/sources.list.d/google-talkplugin.list" do
+	source "google-talkplugin.list"
+	owner "root"
+	group "root"
+	mode 0644
+	action :create_if_missing
+	notifies :run, "execute[update]", :immediate
+end
+
+package "google-talkplugin" do
+	action :install
+end
