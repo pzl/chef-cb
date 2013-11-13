a9ad6506f3568cbc94f67bd7e6864e287b72aeb5
pzl <dan@panzarel.la>
2013-11-13 16:42:59 -0500

    simple chrome install & bookmark save

--- cookbooks/chrome/recipes/default.rb
+++ cookbooks/chrome/recipes/default.rb
@@ -1,6 +1,36 @@
 #chrome yay
-cb-x-www-browser-pipemenu --install-google-chrome
+google_key_url_1='https://dl-ssl.google.com/linux/linux_signing_key.pub'
+google_key_url_2='http://packages.crunchbang.org/waldorf-files/apt-keys/google-chrome.key'
 
-talk plugin.deb
+execute "install-google-key" do
+	command "wget -O- #{google_key_url_1} | apt-key add -"
+	not_if { ::File.exists? "/etc/apt/sources.list.d/google-chrome.list" }
+end
 
-#configs
+cookbook_file "/etc/apt/sources.list.d/google-chrome.list" do
+	source "google-chrome.list"
+	owner "root"
+	group "root"
+	mode 0644
+	notifies :run, "execute[update]", :immediate
+end
+
+execute "update" do
+	command "apt-get update"
+	action :nothing
+end
+
+package "google-chrome-stable" do
+	action :install
+end
+
+#add more configs
+
+cookbook_file "/home/dan/.config/google-chrome/Default/Bookmarks" do
+	source "bookmark"
+	owner "dan"
+	group "dan"
+	mode 0600
+end
+
+#talk plugin.deb
