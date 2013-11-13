87518577e6a1fbf03d43522767b2548213de65b6
pzl <dan@panzarel.la>
2013-11-13 10:37:56 -0500

    skip calibre when in bin, installed properly

--- cookbooks/calibre/recipes/default.rb
+++ cookbooks/calibre/recipes/default.rb
@@ -1,12 +1,12 @@
 #calibre
+#todo: move to attributes?
 fp = "#{Chef::Config['file_cache_path']}/calibre_install"
 calibre_path = "/usr/local"
 
 remote_file fp do
 	source "http://status.calibre-ebook.com/linux_installer"
 	mode 0755
-	action :create_if_missing
-	not_if { ::File.exists?(calibre_path+"/calibre") }
+	not_if { ::File.exists?("/usr/bin/calibre") }
 	notifies :run, "execute[python-calibre]", :immediately
 end
 
