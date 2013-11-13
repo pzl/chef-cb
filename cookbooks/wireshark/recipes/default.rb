e87f689d52c8033dea9b04deaa3258e5aecd8f6a
pzl <dan@panzarel.la>
2013-11-13 13:58:54 -0500

    wireshark with reconfiguring

--- cookbooks/wireshark/recipes/default.rb
+++ cookbooks/wireshark/recipes/default.rb
@@ -1,5 +1,20 @@
-package 'wireshark-common'
+package "wireshark-common" do
+	notifies :run, "execute[wireshark-reconfigure]", :immediately
+	notifies :modify, "group[wireshark]", :immediately
+end
 
-sudo dpkg-reconfigure wireshark-common
-sudo usermod -a -G wireshark dan
-sudo chmod +x /usr/bin/dumpcap
+execute "wireshark-reconfigure" do
+	command <<-EOH
+	echo "set wireshark-common/install-setuid true" | debconf-communicate
+	dpkg-reconfigure -fnoninteractive wireshark-common
+	EOH
+	action :nothing
+end
+
+group "wireshark" do
+	append true
+	members "dan"
+	action :nothing
+end
+
+#group change doesn't take effect until restart
