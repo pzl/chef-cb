758783699deee3ef4509ad8b1a08bf564f9ab95e
pzl <dan@panzarel.la>
2013-11-12 21:49:41 -0500

    add wheezy-backports and update

--- cookbooks/crunch/recipes/default.rb
+++ cookbooks/crunch/recipes/default.rb
@@ -1,9 +1,22 @@
-#cat lib/sources.list | sudo tee -a /etc/apt/sources.list
-#sudo apt-get update
-#sudo apt-get upgrade
-#sudo apt-get dist-upgrade
+#Crunchbang-specific setup
 
+#add backports to sources
+cookbook_file "/etc/apt/sources.list.d/wheezy-backports.list" do
+	source "wheezy-backports.list"
+	mode 0644
+	owner "root"
+	group "root"
+	#notify other rsrc that backports are enabled? or test for backports in them
+end
+#update sources
+bash "apt update" do
+	code "apt-get update"
+end
+
+#move to a dotfiles recipe?
 #ln -s ~/.bashrc lib/dotfiles/.bashrc
+
+
 #ln -s ~/.config/openbox/autostart lib/autostart
 
 
