f312b1d6892b23115c55d42c4e048724fcfa4759
pzl <dan@panzarel.la>
2013-11-13 18:32:30 -0500

    start graphics drivers

--- cookbooks/gfx/recipes/default.rb
+++ cookbooks/gfx/recipes/default.rb
@@ -1,2 +1,46 @@
+smxi_src = "http://smxi.org/smxi.zip"
+smxi_path = "/usr/local/"
+
+
 #detect AMD vs nvidia
-#smxi?
+execute "graphics detection" do
+	command "lspci | grep VGA"
+	action :nothing
+end
+
+#if nvidia
+#package 'module-assistant'
+#package 'nvidia-kernel-common'
+execute "ma nvidia-kernel" do
+	command "m-a auto-install nvidia-kernel-common"
+	action :nothing
+end
+#package "nvidia-glx"
+#package "nvidia-xconfig"
+#package "nvidia-settings"
+execute "nvidia-xconfig" do
+	command "nvidia-xconfig"
+	action :nothing
+end
+
+
+#if AMD: smxi
+package "unzip"
+package "dkms"
+package "linux-headers-$(uname -r)"
+remote_file smxi_path+"smxi.zip" do
+	source smxi_src
+	owner "root"
+	group "root"
+	mode 0644
+	backup 0
+	action :nothing
+end
+execute "unzip smxi" do
+	cwd smxi_path
+	command <<-EOH
+		unzip smxi.zip
+		rm -rf smxi.zip
+	EOH
+	action :nothing
+end
