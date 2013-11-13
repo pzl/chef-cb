0e815250889809307446cbf2acb8330cc3898f5c
pzl <dan@panzarel.la>
2013-11-13 11:06:05 -0500

    mongo install

--- cookbooks/mongo/recipes/default.rb
+++ cookbooks/mongo/recipes/default.rb
@@ -1,1 +1,24 @@
-#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10; sudo touch /etc/apt/sources.list.d/10gen.list; echo "deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen" | sudo tee -a /etc/apt/sources.list.d/10gen.list; sudo apt-get update; sudo apt-get install mongodb-10gen;
+#maybe use apt LWRP in the future
+
+execute "install-mongo-key" do
+	command "apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10"
+	not_if { ::File.exists? "/etc/apt/sources.list.d/10gen.list" }
+end
+
+cookbook_file "/etc/apt/sources.list.d/10gen.list" do
+	source "10gen.list"
+	mode 0644
+	owner "root"
+	group "root"
+	action :create_if_missing
+	notifies :run, "execute[update]", :immediately
+end
+
+execute "update" do
+	command "apt-get update"
+	action :nothing
+end
+
+package "mongodb-10gen" do
+	action :install
+end
