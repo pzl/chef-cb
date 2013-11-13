65ee161cf3879cf07fe5a55e150bedf2e4bff213
pzl <dan@panzarel.la>
2013-11-13 16:05:21 -0500

    moc config

--- cookbooks/moc/recipes/default.rb
+++ cookbooks/moc/recipes/default.rb
@@ -1,3 +1,11 @@
 package 'moc'
-
-#.moc folder
+remote_directory "/home/dan/.moc" do
+	source ".moc"
+	files_owner "dan"
+	files_group "dan"
+	files_backup 0
+	files_mode 0600
+	mode 0755
+	owner "dan"
+	group "dan"
+end
