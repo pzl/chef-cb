ec58f89b2496da4e8f5702fc40ec7c2afdb3a76d
pzl <dan@panzarel.la>
2013-11-13 22:13:03 -0500

    remote_dir bugs that kept re-triggering .conky

--- cookbooks/conky/recipes/default.rb
+++ cookbooks/conky/recipes/default.rb
@@ -1,18 +1,65 @@
 #.conky
-remote_directory "/home/dan/.conky" do
-	files_group "dan"
-	files_owner "dan"
-	files_mode 0600
-	mode 0755
-	owner "dan"
-	group "dan"
-	files_backup 0
-	source ".conky"
-	action :create_if_missing
+
+location = "/home/dan/"
+dirs = [
+	".conky",
+	".conky/lua",
+	".conky/output",
+	".conky/scripts"
+]
+files = [
+	".conky/conky-bar",
+	".conky/conky-bg",
+	".conky/conky-click",
+	".conky/conky-lua",
+	".conky/dzen-demo",
+	".conky/lua/circ.lua",
+	".conky/lua/clicky.lua",
+	".conky/lua/demo.lua",
+	".conky/lua/mail.lua",
+	".conky/lua/start.lua",
+	".conky/output/clicker",
+	".conky/output/ip",
+	".conky/output/sensors.txt",
+]
+
+dirs.each do |dir|
+	directory location+dir do
+		owner "dan"
+		group "dan"
+		mode 0755
+	end
 end
 
-file "/home/dan/.conky/sensors.sh" do
+files.each do |f|
+	cookbook_file location+f do
+		source f
+		owner "dan"
+		group "dan"
+		mode 0600
+		backup false
+	end
+end
+
+
+#remote_directory "/home/dan/.conky/" do
+#	path "/home/dan/.conky"
+#	files_group "dan"
+#	files_owner "dan"
+#	files_mode 0600
+#	mode 0755
+#	owner "dan"
+#	group "dan"
+#	files_backup 0
+#	source ".conky"
+#	action :create_if_missing
+#end
+
+cookbook_file location+".conky/scripts/sensors.sh" do
+	source ".conky/scripts/sensors.sh"
 	mode 0700
+	owner "dan"
+	group "dan"
 end
 
 #cron
