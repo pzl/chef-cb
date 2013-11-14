9e7d23f1a63f38bec4f689569dd69502df101f3c
pzl <dan@panzarel.la>
2013-11-14 10:35:27 -0500

    rigol udev rules

--- cookbooks/rigol/recipes/default.rb
+++ cookbooks/rigol/recipes/default.rb
@@ -1,6 +1,18 @@
 #Rigol scope
 #todo: target product instead of whole driver
-sudo cp "lib/10-Rigol.rules" "/etc/udev/rules.d/"; sudo chown root "/etc/udev/rules.d/10-Rigol.rules"; sudo chmod 644 "/etc/udev/rules.d/10-Rigol.rules";
-sudo udevadm control --reload-rules; sudo udevadm trigger;
 
-#add func gen
+cookbook_file "/etc/udev/rules.d/10-Rigol.rules" do
+	source "10-Rigol.rules"
+	owner "root"
+	group "root"
+	mode 0644
+	notifies :run, "execute[udev-rigol]", :immediately
+end
+
+execute "udev-rigol" do
+	command <<-EOH
+		udevadm control --reload-rules
+		udevadm trigger
+	EOH
+	action :nothing
+end
