ab39151e48d27d1eae48529d579094c7b06daab7
pzl <dan@panzarel.la>
2013-11-14 10:46:39 -0500

    tint2 config

--- cookbooks/theme/recipes/default.rb
+++ cookbooks/theme/recipes/default.rb
@@ -1,5 +1,22 @@
 #flatstudio
+
+
 #tint2
+cookbook_file "/home/dan/.config/tint2/tint2rc" do
+	source "tint2rc"
+	owner "dan"
+	group "dan"
+	mode 0644
+	#notifies :run, "execute[tint2restart]", :immediately
+end
+#bug: still runs as root :[
+#execute "tint2restart" do
+#	command "tint2restart"
+#	user "dan"
+#	action :nothing
+#end
+
+
 #terminator config
 #compton
 #background
