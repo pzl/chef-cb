1d47959a7e01d1fe7a379821060a8ec5f4e89580
pzl <dan@panzarel.la>
2013-11-14 11:01:26 -0500

    gtk settings and wallpaper

--- cookbooks/theme/recipes/default.rb
+++ cookbooks/theme/recipes/default.rb
@@ -1,6 +1,36 @@
 #flatstudio
 
 
+cookbook_file "/home/dan/.config/gtk-3.0/settings.ini" do
+	source "gtk-3-settings"
+	owner "dan"
+	group "dan"
+	mode 0644
+end
+
+#wallpapers
+cookbook_file "/home/dan/.config/nitrogen/bg-saved.cfg" do
+	source "bg-saved.cfg"
+	owner "dan"
+	group "dan"
+	mode 0644
+end
+
+cookbook_file "/home/dan/.config/nitrogen/nitrogen.cfg" do
+	source "nitrogen.cfg"
+	owner "dan"
+	group "dan"
+	mode 0644
+end
+
+cookbook_file "/home/dan/images/wallpapers/6WDf03g.jpg" do
+	source "6WDf03g.jpg"
+	owner "dan"
+	group "dan"
+	mode 0644
+end
+
+
 #tint2
 cookbook_file "/home/dan/.config/tint2/tint2rc" do
 	source "tint2rc"
