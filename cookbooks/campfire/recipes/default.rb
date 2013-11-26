d9b58d0be475796393778f4a464acd541e86bb67
pzl <dan@panzarel.la>
2013-11-26 13:38:37 -0500

    switch to weechat, initial config

--- cookbooks/campfire/recipes/default.rb
+++ cookbooks/campfire/recipes/default.rb
@@ -35,7 +35,76 @@ end
 #	options "-t wheezy-backports"
 #end
 
-#future: could compile with explicitly needed packages
+
+#todo move to .confid/weechat using weechat -d (need to set alias in bash from here?)
+directory "#{node[:user][:home]}/.weechat" do
+	owner node[:user][:name]
+	group node[:user][:name]
+	mode 0755
+	action :create
+end
+
+
+#buffers.pl
+# alternatively find a way to use `weechat -r` to run /script add on these
+directory "#{node[:user][:home]}/.weechat/perl" do
+	owner node[:user][:name]
+	group node[:user][:name]
+	mode 0755
+	action :create
+end
+# when creating recursively, chown only applies to leaf!
+directory "#{node[:user][:home]}/.weechat/perl/autoload" do
+	owner node[:user][:name]
+	group node[:user][:name]
+	mode 0755
+	action :create
+end
+remote_file "#{node[:user][:home]}/.weechat/perl/buffers.pl" do
+	source "http://www.weechat.org/files/scripts/buffers.pl"
+	owner node[:user][:name]
+	group node[:user][:name]
+	mode 0755
+	action :create
+end
+link "#{node[:user][:home]}/.weechat/perl/autoload/buffers.pl" do
+	to "#{node[:user][:home]}/.weechat/perl/buffers.pl"
+	owner node[:user][:name]
+	group node[:user][:name]
+end
+template "#{node[:user][:home]}/.weechat/weechat.conf" do
+	source ".weechat/weechat.conf"
+	mode 0644
+	owner node[:user][:name]
+	group node[:user][:name]
+	backup false
+	variables({
+		:nick => node[:user][:name] #change
+	})
+end
+template "#{node[:user][:home]}/.weechat/buffers.conf" do
+	source ".weechat/buffers.conf"
+	mode 0644
+	owner node[:user][:name]
+	group node[:user][:name]
+	backup false
+end
+template "#{node[:user][:home]}/.weechat/irc.conf" do
+	source ".weechat/weechat.conf"
+	mode 0644
+	owner node[:user][:name]
+	group node[:user][:name]
+	backup false
+	variables({
+		:nick => node[:user][:name], #change
+		:domain => "sample",
+		:key => "1234567890"
+	})
+end
+
+#more config
+#http://dotshare.it/dots/140/
+#http://dotshare.it/dots/395/
 
 =begin
 directory "#{node[:user][:home]}/.irssi" do
