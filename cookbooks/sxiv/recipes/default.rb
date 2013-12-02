adef6561e5faba0ba67c40ad82964f17c9d1be8b
pzl <dan@panzarel.la>
2013-12-01 19:57:50 -0500

    sxiv -- simple image viewer

---
+++ cookbooks/sxiv/recipes/default.rb
@@ -0,0 +1,17 @@
+package 'libx11-dev'
+package 'libimlib2-dev'
+
+git "#{Chef::Config[:file_cache_path]}/sxiv" do
+	repository "https://github.com/muennich/sxiv.git"
+	action :checkout
+	notifies :run, "execute[install sxiv]", :immediately
+end
+
+execute "install sxiv" do
+	cwd "#{Chef::Config[:file_cache_path]}/sxiv"
+	command <<-EOH
+		make
+		make install
+	EOH
+	action :nothing
+end
