68473ac3b893cbe789236623d89ffd3fc7474930
pzl <dan@panzarel.la>
2013-12-05 18:17:02 -0500

    udev for ttyS* to be group `dan`
    closes #2

--- cookbooks/avr/recipes/default.rb
+++ cookbooks/avr/recipes/default.rb
@@ -10,7 +10,23 @@ template "#{node[:user][:home]}/.avrduderc" do
 	group node[:user][:name]
 end
 
+template "/etc/udev/rules.d/99-serial.rules" do
+	source "99-serial.rules"
+	mode 0644
+	variables(
+		:group => node[:user][:name]
+	)
+	notifies :run, "execute[udev avrdude]", :immediately
+end
+
+execute "udev avrdude" do
+	command <<-EOH
+		udevadm control --reload-rules
+		udevadm trigger
+	EOH
+	action :nothing
+end
+
 
 #todo
-#TTYS1 rules & reload udev
 #tolerate usb to serial
